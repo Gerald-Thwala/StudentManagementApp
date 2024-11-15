03638a69d2bb98605d5444bfd634d43ff119b065
Gerald Thwala <thwalag@legal.regn.net>
2024-11-15 20:50:01 +0200

    added DeleteStudentCommand

---
+++ StudentManagementApp/Application/Commands/DeleteStudentCommand.cs
@@ -0,0 +1,50 @@
+﻿using StudentManagementApp.Application.Services;
+using StudentManagementApp.Application.Validators;
+
+namespace StudentManagementApp.Application.Commands;
+
+public class DeleteStudentCommand : ICommand
+{
+    private readonly StudentService _studentService;
+
+    public DeleteStudentCommand(StudentService studentService)
+    {
+        _studentService = studentService;
+    }
+
+    public async Task ExecuteAsync(string args)
+    {
+
+
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please specify--id for edit action.");
+            return;
+        }
+        
+        if (!InputValidator.ValidateStudentId(args, out int id))
+        {
+            Console.WriteLine("Invalid Student ID.");
+            return;
+        }
+
+        var existingStudent = await _studentService.GetStudentByIdAsync(id);
+        if (existingStudent == null)
+        {
+            Console.WriteLine("Student not found.");
+            return;
+        }
+
+        Console.Write($"Are you sure you want to delete {existingStudent.Name} {existingStudent.Surname}? (yes/no): ");
+        string confirmation = Console.ReadLine()!;
+        if (confirmation.ToLower() != "yes")
+        {
+            Console.WriteLine("Operation cancelled.");
+            return;
+        }
+
+        await _studentService.DeleteStudentAsync(id);
+
+        Console.WriteLine("Student deleted successfully.");
+    }
+}
