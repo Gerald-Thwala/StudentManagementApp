8c3efbb3df709032749b59058f960d4ac173d8ea
Gerald Thwala <thwalag@legal.regn.net>
2024-11-15 13:32:07 +0200

    Add CommandFactory class to create command instances based on action strings
    - Implement CommandFactory constructor to initialize service provider and command dictionary.
    - Add "add" command mapping to AddStudentCommand.
    - Implement TryGetCommand method to retrieve command instances from the service provider.

---
+++ StudentManagementApp/Application/Factories/CommandFactory.cs
@@ -0,0 +1,32 @@
+﻿using StudentManagementApp.Application.Interfaces;
+using StudentManagementApp.Application.Commands;
+
+namespace StudentManagementApp.Application.Factories
+{
+    public class CommandFactory : ICommandFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly Dictionary<string, Type> _commands;
+
+        public CommandFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _commands = new()
+        {
+            { "add", typeof(AddStudentCommand) },
+            
+        };
+        }
+
+        public bool TryGetCommand(string action, out ICommand command)
+        {
+            command = null!;
+            if (_commands.TryGetValue(action, out var type))
+            {
+                command = (ICommand)_serviceProvider.GetService(type)!;
+                return command != null;
+            }
+            return false;
+        }
+    }
+}
