ed06aed5c30f864c9b8951a6980404c8d335aec6
Gerald Thwala <thwalag@legal.regn.net>
2024-11-15 17:10:53 +0200

    documentation for ExecuteAsync method

--- StudentManagementApp/Application/Commands/ICommand.cs
+++ StudentManagementApp/Application/Commands/ICommand.cs
@@ -1,5 +1,10 @@
 ﻿namespace StudentManagementApp.Application.Commands;
 public interface ICommand
 {
+    /// <summary>
+    /// Executes the command with the given arguments.
+    /// </summary>
+    /// <param name="args">The arguments passed to the command.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task ExecuteAsync(string[] args);
 }
